c326d577521c876bd27c368fe1de7f0a08135681
LilSpen <116097698+LilSpen@users.noreply.github.com>
2024-09-11 10:49:01 -0600

    Completed Week2 Assignment
    Created week-2 folder, created student-info component, displayed student-info component on week-2 page.
    
    Created landing page for assignments in the future.

--- app/page.js
+++ app/page.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import StudentInfo from "./week-2/student-info";
+
 export default function Home() {
     return (
       <main>
-
+        <h1>CPRG 306 Assignments</h1>
+        
+        <li><Link href="week-2">Week 2 Assignment</Link></li>
+        <li><Link href="week-3">Week 3 Assignment</Link></li>
+        <li><Link href="week-4">Week 4 Assignment</Link></li>
+        <li><Link href="week-5">Week 5 Assignment</Link></li>
+        <li><Link href="week-6">Week 6 Assignment</Link></li>
+        <li><Link href="week-7">Week 7 Assignment</Link></li>
+        <li><Link href="week-8">Week 8 Assignment</Link></li>
+        <li><Link href="week-9">Week 9 Assignment</Link></li>
+        <li><Link href="week-10">Week 10 Assignment</Link></li>
+        
       </main>
     );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo (){
+    return(
+    <main>
+        <p>Evan Spencer</p>
+        <p>evan.spencer@edu.sait.ca</p>
+        <Link href="https://github.com/LilSpen/cprg306-assignments">https://github.com</Link>
+    </main>
+    );
+}
