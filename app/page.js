f1ee78f555b8f4ffb6e17def6461a5f2834a81f3
LilSpen <116097698+LilSpen@users.noreply.github.com>
2024-09-24 14:28:22 -0600

    Root Page Styling
    Added styling for the root page, removed all of the unused clutter.

--- app/page.js
+++ app/page.js
@@ -2,26 +2,25 @@ import Link from "next/link";
 import StudentInfo from "./week-2/student-info";
 
 export default function Home() {
+  let linkStyles = "underline text-cyan-300 hover:text-cyan-100"; // Light blue text with hover effect
 
-  let linkStyles = "underline text-cyan-600 hover:text-cyan-300"
-    return (
-      <main>
-        <h1>CPRG 306 Assignments</h1>
-        
-        <li><Link href="week-2" className={linkStyles}>Week 2 Assignment</Link></li>
-
-
-        <li><Link href="week-3/objects" className={linkStyles}>Week 3 Objects</Link></li>
-        <li><Link href="week-3/props" className={linkStyles}>Week 3 Props</Link></li>
-
-        <li><Link href="week-4"  className={linkStyles}>Week 4 Assignment</Link></li>
-        <li><Link href="week-5"  className={linkStyles}>Week 5 Assignment</Link></li>
-        <li><Link href="week-6"  className={linkStyles}>Week 6 Assignment</Link></li>
-        <li><Link href="week-7"  className={linkStyles}>Week 7 Assignment</Link></li>
-        <li><Link href="week-8"  className={linkStyles}>Week 8 Assignment</Link></li>
-        <li><Link href="week-9"  className={linkStyles}>Week 9 Assignment</Link></li>
-        <li><Link href="week-10" className={linkStyles}>Week 10 Assignment</Link></li>
-        
-      </main>
-    );
+  return (
+    <main
+      style={{
+        backgroundColor: "#172544", 
+        minHeight: "100vh", 
+        padding: "20px",
+        paddingTop: "100px",
+        paddingLeft: "300px"
+      }}
+     
+    >
+      <h1 style={{fontSize: "60px", fontWeight: "bold", color: "cyan"}}>CPRG 306 Assignments</h1> 
+      
+      <ul> 
+        <li><Link href="week-2" className="text-cyan-500 hover:text-indigo-500 hover:underline text-xl">Week 2 Assignment</Link></li>
+      </ul>
+    </main>
+  );
 }
+
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -4,7 +4,7 @@ export default function Page() {
     return (
         <main style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh", backgroundColor: "lightblue" }}>
             <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-                <h2 style={{ color: "purple" }}>Week 2 Assignment</h2>
+                <h2 style={{ color: "purple" }}>Shopping List</h2>
                 <StudentInfo/>
             </div>
         </main>
